2f707a8e8905e55c2d83b52dcbe061025494eb05
정다운 <daunny@jeongdaun-ui-MacBookPro.local>
2021-04-18 22:03:29 +0900

    Update Admin.py - Update Table on Admin Pages

--- mysite/bookmark/admin.py
+++ mysite/bookmark/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bookmark.models import Bookmark
 
-# Register your models here.
+# 데코레이터 (기존 함수 수정하지 않고, 추가 기능 구현 시)
+# admin.register(Bookmark) 추가 기능 확장 구현
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
+
+# Register your models here. (admin 사이트에 테이블 반영)

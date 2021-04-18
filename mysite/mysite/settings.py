b4bab7a93f700bf7c8c43ab22e9879ade8d3e1e6
정다운 <daunny@jeongdaun-ui-MacBookPro.local>
2021-04-18 21:38:42 +0900

    Add .gitignore & Update Settings.py (2) - delete secret key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w%qar*ib1fz**e(mg69rtr)5tsimo&r$*u$0aijvv61c07dei8'
+SECRET_KEY = 'hello'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
